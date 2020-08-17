0134b8e1b9db035b8768730de7aa78fb8eba9725
calvincurry22 <calvincurry22@gmail.com>
2020-08-17 17:41:35 -0500

    Added PDFLink to allow for downloading pdf docs without viewing them in the DOM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { PDFViewer } from '@react-pdf/renderer';
+import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
 import MyDocument from './components/MyDocument';
 
+export default () => {
+  /*Using PDFDownloadLink allos for 'On the fly' document creation,
+  meaning that it doesn't have to render on screen before being downloaded. */
+  return (
+    <PDFDownloadLink document={<MyDocument />}>
+      Click to download
+    </PDFDownloadLink>
+  )
+};
 
-export default () => (
-  <PDFViewer>
-    <MyDocument />
-  </PDFViewer>
-);
 
 
-export default App;
